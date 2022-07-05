bc9473dcd2b48fd87f1aeabda308c5db81e83a9e
Дмитрий Кутин <TacoKong@yandex.ru>
2022-07-05 23:23:29 +0300

    registered models in admin

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 from . import models
 
 
-# @admin.register(models.User)
-# class UserAdmin(admin.ModelAdmin):
-#     # list_display = ('pk', 'email', 'first_name', 'last_name', 'bio', 'role')
-#     # search_fields = ('first_name', 'last_name')
-#     # list_filter = ('role',)
-#     # list_editable = ('role',)
-#     empty_value_display = '-пусто-'
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'last_name', 'email')
+    # search_fields = ('first_name', 'last_name')
+    list_filter = ('first_name', 'email',)
+    # list_editable = ('role',)
+    empty_value_display = '-пусто-'
 
 
 @admin.register(models.Tag)
@@ -23,17 +23,17 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(models.Ingridient)
 class IngridientAdmin(admin.ModelAdmin):
-    # list_display = ('pk', 'email', 'first_name', 'last_name', 'bio', 'role')
+    list_display = ('pk', 'name', 'measurement_unit')
     # search_fields = ('first_name', 'last_name')
-    # list_filter = ('role',)
+    list_filter = ('name',)
     # list_editable = ('role',)
     empty_value_display = '-пусто-'
 
 
 @admin.register(models.Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    # list_display = ('pk', 'email', 'first_name', 'last_name', 'bio', 'role')
+    list_display = ('pk', 'name', 'author', )
     # search_fields = ('first_name', 'last_name')
-    # list_filter = ('role',)
-    # list_editable = ('role',)
+    list_filter = ('name', 'author', 'tags')
+    # list_editable = ()
     empty_value_display = '-пусто-'

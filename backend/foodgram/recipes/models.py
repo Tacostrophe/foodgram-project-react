3caf886abbf515da865fe28382d46bedf806083a
Дмитрий Кутин <TacoKong@yandex.ru>
2022-07-06 21:42:53 +0300

    first and last name and email are required now

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 # from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
-# from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -15,21 +14,18 @@ class User(AbstractUser):
 #         (MODERATOR, '1'),
 #         (ADMIN, '2'),
 #     )
-#     email = models.EmailField(
-#         'email',
-#         max_length=254,
-#         unique=True
-#     )
-#     first_name = models.CharField(
-#         'first name',
-#         max_length=150,
-#         blank=True,
-#     )
-#     last_name = models.CharField(
-#         'last name',
-#         max_length=150,
-#         blank=True,
-#     )
+    email = models.EmailField(
+        'email',
+        max_length=254,
+    )
+    first_name = models.CharField(
+        'first name',
+        max_length=150,
+    )
+    last_name = models.CharField(
+        'last name',
+        max_length=150,
+    )
 #     bio = models.TextField(
 #         'biography',
 #         blank=True,
@@ -41,28 +37,28 @@ class User(AbstractUser):
 #         choices=Role,
 #         max_length=10,
 #     )
-# 
-#     class Meta:
-#         constraints = [
-#             models.constraints.UniqueConstraint(
-#                 fields=('username', 'email'),
-#                 name='user_email_constraint'
-#             )
-#         ]
-#         ordering = ('-id',)
-# 
+
+    class Meta:
+        constraints = [
+            models.constraints.UniqueConstraint(
+                fields=('username', 'email'),
+                name='user_email_constraint'
+            )
+        ]
+        ordering = ('-id',)
+#
 #     @property
 #     def is_user(self):
 #         return self.role == self.USER
-# 
+#
 #     @property
 #     def is_moderator(self):
 #         return self.role == self.MODERATOR
-# 
+#
 #     @property
 #     def is_admin(self):
 #         return self.role == self.ADMIN
-# 
+#
 #     def permission_level(self) -> int:
 #         return int(self.get_role_display())
 
@@ -73,10 +69,12 @@ class Tag(models.Model):
         'Тэг',
         default='Наименаование тэга',
         max_length=50,
+        unique=True,
     )
     color = models.CharField(
         'Цвет',
         max_length=12,
+        unique=True,
     )
     slug = models.SlugField(unique=True)
 
@@ -175,6 +173,9 @@ class Recipe(models.Model):
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
         ordering = ('name', 'author')
+
+    def __str__(self):
+        return self.name
 
 
 class AmountOfIngridient(models.Model):

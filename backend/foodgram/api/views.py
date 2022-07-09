aa92063d5d57b2ab4cf2f80824f653375374325c
Дмитрий Кутин <TacoKong@yandex.ru>
2022-07-09 23:59:19 +0300

    tag and ingredient viewsets created

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from . import serializers
+from recipes import models
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет для тэгов"""
+    queryset = models.Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class RecipeViewSet():
+    """Вьюсет для рецептов"""
+
+
+class ShoppingCartViewSet():
+    """Вьюсет для списка покупок"""
+
+
+class FavoriteViewSet():
+    """Вьюсет для избранного"""
+
+
+class SubscriptionViewSet():
+    """Вьюсет для подписок"""
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет для ингредиентов"""
+    queryset = models.Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
